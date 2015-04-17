a4a556bec8c1f50b01d09c7d48c5f6a450704375
Jeff Brandon <jdbrando@andrew.cmu.edu>
2015-04-16 20:04:39 -0400

    added tarbal creating script

---
+++ maketar.sh
@@ -0,0 +1,12 @@
+cp eval.h ./handin
+cp eval.c ./handin
+cp tables.h ./handin
+cp tables.c ./handin
+cp bad*.simple ./handin
+cp good*.simple ./handin
+cp *.in ./handin
+cp *.out ./handin
+cp *.err ./handin
+cp control*.simple ./handin
+tar czvf handin.tar ./handin
+tar tvf handin.tar
